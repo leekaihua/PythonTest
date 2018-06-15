49799939581972c59aeb86b818d3352f565041f9
likaihua01 <likaihua01@baidu.com>
2018-06-15 11:16:10 +0800

    修改添加come on

--- test.py
+++ test.py
@@ -5,5 +5,5 @@ import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
-print "Hello Baidu! Dare to excel!"
+print "Hello Baidu! Dare to excel! come on!"
 print "Hello World ,I'm Baidu! How are you?"
